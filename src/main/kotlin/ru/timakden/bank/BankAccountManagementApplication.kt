385447ef00ce2e3fa14aa3070652792b13dbd205
Denis Timakov <timakden88@gmail.com>
2020-04-03 14:54:47 +0300

    Added initial structure, GitHub Actions script

---
+++ src/main/kotlin/ru/timakden/bank/BankAccountManagementApplication.kt
@@ -0,0 +1,11 @@
+package ru.timakden.bank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankAccountManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankAccountManagementApplication>(*args)
+}
---
+++ src/test/kotlin/ru/timakden/bank/BankAccountManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.timakden.bank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankAccountManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
