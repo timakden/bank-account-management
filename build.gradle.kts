741bae9c61770e605cc62cfa7a27dbd9f1f34ace
Denis Timakov <timakden88@gmail.com>
2021-04-16 08:04:50 +0300

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     idea
     id("com.github.ben-manes.versions") version "0.38.0"
-    id("org.springframework.boot") version "2.4.3"
+    id("org.springframework.boot") version "2.4.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.4.31"
-    kotlin("plugin.spring") version "1.4.31"
-    kotlin("plugin.jpa") version "1.4.31"
+    kotlin("jvm") version "1.4.32"
+    kotlin("plugin.spring") version "1.4.32"
+    kotlin("plugin.jpa") version "1.4.32"
 }
 
 group = "ru.timakden"
@@ -38,8 +38,8 @@ dependencies {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
     testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.testcontainers:postgresql:1.15.2")
-    testImplementation("org.testcontainers:junit-jupiter:1.15.2")
+    testImplementation("org.testcontainers:postgresql:1.15.3")
+    testImplementation("org.testcontainers:junit-jupiter:1.15.3")
 }
 
 tasks.withType<Test> {
