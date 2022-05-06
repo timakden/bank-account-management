645e57a237d4b97b098c92c7063c384175e6ce13
Denis Timakov <timakden88@gmail.com>
2022-05-06 08:28:52 +0300

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,20 +4,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     idea
     id("com.github.ben-manes.versions") version "0.42.0"
-    id("org.springframework.boot") version "2.6.6"
+    id("org.springframework.boot") version "2.6.7"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.20"
-    kotlin("plugin.spring") version "1.6.20"
-    kotlin("plugin.jpa") version "1.6.20"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "ru.timakden"
 version = "0.0.1-SNAPSHOT"
-
-java {
-    sourceCompatibility = VERSION_17
-    targetCompatibility = VERSION_17
-}
 
 repositories {
     mavenCentral()
@@ -25,7 +20,7 @@ repositories {
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.boot:spring-boot-dependencies:2.6.6")
+        mavenBom("org.springframework.boot:spring-boot-dependencies:2.6.7")
     }
 }
 
@@ -48,8 +43,14 @@ dependencies {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
     testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.testcontainers:postgresql:1.16.3")
-    testImplementation("org.testcontainers:junit-jupiter:1.16.3")
+    testImplementation("org.testcontainers:postgresql:1.17.1")
+    testImplementation("org.testcontainers:junit-jupiter:1.17.1")
+}
+
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 tasks {
