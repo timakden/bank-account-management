48404095af40ccd2145d9896458c5be083131440
Denis Timakov <timakden88@gmail.com>
2023-07-07 11:04:40 +0300

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
     id("com.github.ben-manes.versions") version "0.47.0"
     id("org.springframework.boot") version "3.1.1"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
-    kotlin("plugin.jpa") version "1.8.22"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.0"
+    kotlin("plugin.jpa") version "1.9.0"
 }
 
 group = "ru.timakden"
