df7c7cea78e40cc43a706c72d8fa8535b9ab94e6
Denis Timakov <timakden88@gmail.com>
2024-01-27 19:25:06 +0300

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     idea
-    id("com.github.ben-manes.versions") version "0.50.0"
-    id("org.springframework.boot") version "3.2.0"
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.21"
-    kotlin("plugin.spring") version "1.9.21"
-    kotlin("plugin.jpa") version "1.9.21"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
+    kotlin("plugin.jpa") version "1.9.22"
 }
 
 group = "ru.timakden"
@@ -19,8 +19,8 @@ repositories {
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.boot:spring-boot-dependencies:3.2.0")
-        mavenBom("org.testcontainers:testcontainers-bom:1.19.3")
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.2.2")
+        mavenBom("org.testcontainers:testcontainers-bom:1.19.4")
     }
 }
 
@@ -30,7 +30,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("io.github.oshai:kotlin-logging-jvm:5.1.1")
+    implementation("io.github.oshai:kotlin-logging-jvm:6.0.3")
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
