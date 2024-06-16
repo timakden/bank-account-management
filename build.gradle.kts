22771065315ee0fa035c1338c37d23e0d06259f5
Denis Timakov <timakden88@gmail.com>
2024-06-16 22:44:03 +0300

    Update Gradle and Kotlin Logging versions
    Updated Gradle from version 8.7 to 8.8 in the gradle-wrapper.properties, build.gradle.kts, and gradle.yml files. Also updated the Kotlin logging dependency from 6.0.9 to 7.0.0 in the build.gradle.kts file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("io.github.oshai:kotlin-logging-jvm:6.0.9")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
@@ -63,7 +63,7 @@ tasks {
         useJUnitPlatform()
     }
     wrapper {
-        gradleVersion = "8.7"
+        gradleVersion = "8.8"
     }
 }
 
