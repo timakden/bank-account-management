51183ffb38546a7cef021b72951542a1eb41a806
Denis Timakov <timakden88@gmail.com>
2024-07-19 18:41:29 +0300

    Update Spring Boot and Testcontainers versions
    The versions of Spring Boot and Testcontainers have been updated. The Spring Boot version has been bumped from 3.3.1 to 3.3.2 and the Testcontainers' version has been raised from 1.19.8 to 1.20.0. These updates ensure we are utilizing the latest available patches and improvements.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 plugins {
     idea
     id("com.github.ben-manes.versions") version "0.51.0"
-    id("org.springframework.boot") version "3.3.1"
+    id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.spring") version "2.0.0"
@@ -19,8 +19,8 @@ repositories {
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.1")
-        mavenBom("org.testcontainers:testcontainers-bom:1.19.8")
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.2")
+        mavenBom("org.testcontainers:testcontainers-bom:1.20.0")
     }
 }
 
